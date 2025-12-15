1b0b791baca77390c8bb23a33783e53396b93f13
Mithian Acuna <59997173+Butterpod@users.noreply.github.com>
2025-12-15 12:21:00 +0100

    Daily Code Challenge 15/12/2025 - Ajout de ReadMe pour les consignes + les tests

---
+++ Python/14-12-2025/14-12-2025.py
@@ -0,0 +1,16 @@
+def title_case(title):
+    li_split = title.split()
+    li_res = []
+
+    for i in li_split:
+        li_res.append(i.capitalize())
+
+    res = " ".join(li_res)
+
+    return res
+
+
+title_case("hello world")  # "Hello World".
+title_case("the quick brown fox")  # "The Quick Brown Fox"
+title_case("JAVASCRIPT AND PYTHON")  # "Javascript And Python"
+title_case("AvOcAdO tOAst fOr brEAkfAst")  # "Avocado Toast For Breakfast".
