2da5da2231d713e50d199cc8e832b9be9a99c125
Mithian Acuna <59997173+Butterpod@users.noreply.github.com>
2026-01-18 17:48:18 +0100

    Daily Code Challenge 16/01/2026 - Ajout de ReadMe pour les consignes + les tests

---
+++ Python/2026/16-01-2026/16-01-2026.py
@@ -0,0 +1,11 @@
+# Import math Library
+import math 
+def is_integer_hypotenuse(a, b):    return (math.sqrt(pow(a,2)+pow(b,2))  ).is_integer()
+
+
+is_integer_hypotenuse(3, 4)
+is_integer_hypotenuse(2, 3)
+is_integer_hypotenuse(5, 12)
+is_integer_hypotenuse(10, 10)
+is_integer_hypotenuse(780, 1040)
+is_integer_hypotenuse(250, 333)
