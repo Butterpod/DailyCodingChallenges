626fdd84fe169228cf920f6102addcc8f1130d66
Mithian Acuna <59997173+Butterpod@users.noreply.github.com>
2026-01-25 15:20:36 +0100

    Daily Code Challenge 25/01/2026 - Ajout de ReadMe pour les consignes + les tests

---
+++ Python/2026/25-01-2026/25-01-2026.py
@@ -0,0 +1,13 @@
+def scale_image(size, scale):
+    print(size)
+    print(scale)
+    a=size.split('x')
+    print(int(a[0])*2)
+    print(f"{int(a[0])*scale:n}x{int(a[1])*scale:n}")
+
+    return f"{int(a[0])*scale:n}x{int(a[1])*scale:n}"
+
+scale_image("800x600", 2) #"1600x1200".
+scale_image("100x100", 10) #"1000x1000".
+scale_image("1024x768", 0.5) # "512x384".
+scale_image("300x200", 1.5) # "450x300".
